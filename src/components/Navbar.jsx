febec39875827c0562833a9ce8ada23716734dc1
Your Name <you@example.com>
2026-08-31 21:42:07 +0530

    navchanged

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
             onClick={() => {
               (navigate("/home"), setClickedbtn("home"));
             }}
-          > new
+          > new valid chanduru
             Home
           </button>
 
