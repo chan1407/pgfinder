28db6792e2074dc3a0abce75d46a1befaa708462
Your Name <you@example.com>
2026-09-01 12:55:56 +0530

    bootsstrap

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import "./index.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
