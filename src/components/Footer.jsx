c5f93ed8a139be1eb0126f210ebaf5fac7d6430a
Your Name <you@example.com>
2026-09-01 08:03:08 +0530

    rechanged

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
         <div className="footer-links">
           <h4>Quick Links</h4>
 
-          <p>Home hi</p>
+          <p>Home</p>
           <p>Find PG</p>
           <p>Favourites</p>
         </div>
